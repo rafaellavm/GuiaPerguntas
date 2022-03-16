6bb8d0dd1bd2a9e8324f9bfd924f36ca296d8473
Rafaela <rafaela.vm@gmail.com>
2022-03-16 19:02:06 -0300

    26 - Estruturas de repetição com EJS

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ app.get("/:nome/:lang", (req, res) => {
 
     var nome = "Rafaela Marraschi";
     var lang = "Javascript";
+
+    var produtos = [
+        { nome: "Doritos", preco: 3.14 },
+        { nome: "Coca-Cola", preco: 5 },
+        { nome: "Leite", preco: 7 }
+    ]
+
     res.render("index", {
         nome: req.params.nome,
         lang: req.params.lang,
         empresa: "Guia do programador",
         inscritos: 8000,
-        msg: false
+        msg: false,
+        produtos: produtos
     });
 });
 
